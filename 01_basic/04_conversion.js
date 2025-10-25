ac76d501352bf8c8e3d85a1cc740642c3e219401
Rahul <rahulyadavvns93@gmail.com>
2025-10-25 10:34:50 +0530

    operation

--- 01_basic/04_conversion.js
+++ 01_basic/04_conversion.js
@@ -6,5 +6,32 @@
  console.log(newAger)
     console.log(typeof(newAger))
 
-    
+
 // 33abr= show NaN not a number
+
+// ****************************Operation*******************************
+let val=3;
+let negvale=-val
+console.log(negvale)
+
+console.log(2+2)
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);//modulus operator
+console.log(2**3);//exponentiation operator
+
+let str1="hello"
+let str2="rahul"
+console.log(str1+" " +str2)
+console.log(1+"2")//string concatenation
+console.log("1"+2)
+
+console.log(2+3+4*5+9/3-1)//operator precedence(BODMAS rule)  this is wrong way to do alwayes write with bracket and sprteatly
+
+let a=100;
+console.log(a++)//post increment 
+
+console.log(++a)//pre increment
+console.log(a--)//post decrement
+console.log(--a)//pre decrement
