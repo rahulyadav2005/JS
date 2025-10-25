bd28736f4a06b33b6488007a21c5bae3c1aa75f1
Rahul <rahulyadavvns93@gmail.com>
2025-10-25 09:05:16 +0530

    know let var const

--- 01_basic/variables.js
+++ 01_basic/variables.js
@@ -1,9 +1,15 @@
 const accountId=7090
 let accountMail ="rahulvnss@zohomail.in"
-var AccoountPassword="Rahul1234@#"
+var AccoountPassword="Rahul1234@#" //var ko  ek baar change karene per sare var change ho jate the  also use let or const because issue in block or functional scope 
 AccountCity="varanasi"
 
 // accountId=889
 
 // console.log(accountId);
+AccountCity="delhi"
+let accountState;
+
 console.log(accountId)
+console.log(AccountCity)
+
+console.table({accountId,accountMail,AccoountPassword,AccountCity,accountState})//table method use karne se data table format me show hota hai
