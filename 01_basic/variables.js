6dff71bb0868f0e2bb4471e6864345a31265d683
Rahul <rahulyadavvns93@gmail.com>
2025-10-25 05:52:45 +0530

    variable file

---
+++ 01_basic/variables.js
@@ -0,0 +1,9 @@
+const accountId=7090
+let accountMail ="rahulvnss@zohomail.in"
+var AccoountPassword="Rahul1234@#"
+AccountCity="varanasi"
+
+// accountId=889
+
+// console.log(accountId);
+console.log(accountId)
