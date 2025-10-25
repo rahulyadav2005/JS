13bd8fc49f44f02f81a4c3da9db6c7c8c7a6aa17
Rahul <rahulyadavvns93@gmail.com>
2025-10-25 10:15:55 +0530

    conversion of data type

---
+++ 01_basic/04_conversion.js
@@ -0,0 +1,10 @@
+ let age="24"
+ console.log(age)
+ console.log(typeof(age))
+
+ let newAger=Number(age)
+ console.log(newAger)
+    console.log(typeof(newAger))
+
+    
+// 33abr= show NaN not a number
