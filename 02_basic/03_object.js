1755463d0a4ac848acba253422137198c1bdf1f8
Rahul <rahulyadavvns93@gmail.com>
2025-10-27 18:34:46 +0530

    object

---
+++ 02_basic/03_object.js
@@ -0,0 +1,19 @@
+// singlrton object
+//
+
+//object literal syntax
+//  const sym=symbol("key1");
+ 
+const user ={
+    name:"rahul ",
+    // [mysym]:"suman",
+    age:20,
+    email:"rahul.com"
+
+}
+console.log([user])
+console.table([user]);
+// console.log(user[mysym]);
+
+
+    
